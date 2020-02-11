d508cadfd2d4963a1a9d0320be7e419a2a2bd252
Bruno Silva <brunosilvinosilva@gmail.com>
2020-02-11 13:58:28 +0000

    greet and name

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,3 +1,22 @@
-def greet
-    puts "Welcome to the Continent"
+class CommandLineInterface
+    def greet
+        puts "==============================="
+        puts    "Welcome to the Continent"
+        puts "==============================="
+    end 
+
+    def name
+        puts "==================="
+        puts "Enter your username: "
+        puts "==================="
+        ans = gets.chomp
+        @new_user = User.find_or_create_by(name: ans)
+    end 
+
+
 end 
+
+
+
+
+
