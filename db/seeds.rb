4d80368f6aa92e1cb1825a1763cbd80d629a0ea8
Bruno Silva <brunosilvinosilva@gmail.com>
2020-02-12 17:16:10 +0000

    updated CLI methods, specifically 2 and 3 - missing user id for method 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@ c2 = Character.find_or_create_by(name: "Yennefer Of Vengerberg", species: "Mixed
 w1 = Weapon.find_or_create_by(name: "Steel Sword", category: "Bladed Melee", health: 0, strength: 50, defence: 50)
 w2 = Weapon.find_or_create_by(name: "Silver Sword", category: "Bladed Melee", health: 0, strength: 25, defence: 100)
 w3 = Weapon.find_or_create_by(name: "Chaos", category: "Magic", health: -50, strength: 100, defence: 100)
+w4 = Weapon.find_or_create_by(name: "Crossbow", category: "Range", health: 25, strength: 0, defence: 25)
+w5 = Weapon.find_or_create_by(name: "Shield", category: "Melee", health: 100, strength: -50, defence: 200)
+w6 = Weapon.find_or_create_by(name: "Portals", category: "Magic", health: 0, strength: 0, defence: 0)
+
 
 cw1 = CharacterWeapon.find_or_create_by(character_id: c1.id, weapon_id: w1.id)
 cw2 = CharacterWeapon.find_or_create_by(character_id: c1.id, weapon_id: w2.id)
--- lib/CLI.rb
+++ lib/CLI.rb
@@ -15,9 +15,9 @@ class CommandLineInterface
     end 
 
     def name
-        puts "===================="
-        puts "Enter your username: "
-        puts "===================="
+                puts "===================="
+                puts "Enter your username: "
+                puts "===================="
         ans = gets.chomp
         @new_user = User.find_or_create_by(name: ans)
     end 
@@ -25,15 +25,15 @@ class CommandLineInterface
     def menu 
         puts "
             1. Create a new character 
-            2. View your brave and mighty warriors 
+            2. View your character Collection
             3. Gear Up
-            4. Kill yourself
+            4. End your suffering 
             5. Exit
         "
 
-        puts "=================================================="
-        puts "Please select from one of the options above: (1-5)"
-        puts "=================================================="
+            puts "=================================================="
+            puts "Please select from one of the options above: (1-5)"
+            puts "=================================================="
 
         input = gets.chomp.to_i
         case input 
@@ -59,25 +59,25 @@ class CommandLineInterface
     end
 
     def new_character
-        puts "---------------------------------------"
-        puts "Please enter the name of your character"
-        puts "---------------------------------------"
+            puts "---------------------------------------"
+            puts "Please enter the name of your character"
+            puts "---------------------------------------"
         ans_name = gets.chomp 
-        puts "------------------------------------------"
-        puts "Please enter the species of your character"
-        puts "------------------------------------------"
+            puts "------------------------------------------"
+            puts "Please enter the species of your character"
+            puts "------------------------------------------"
         ans_species = gets.chomp 
-        puts "-------------------------------------------------"
-        puts "Please enter the health of your character (0-75)"
-        puts "-------------------------------------------------"
+            puts "-------------------------------------------------"
+            puts "Please enter the health of your character (0-75)"
+            puts "-------------------------------------------------"
         ans_health = gets.chomp 
-        puts "---------------------------------------------------"
-        puts "Please enter the strength of your character (0-75)"
-        puts "---------------------------------------------------"
+            puts "---------------------------------------------------"
+            puts "Please enter the strength of your character (0-75)"
+            puts "---------------------------------------------------"
         ans_strength = gets.chomp 
-        puts "--------------------------------------------------"
-        puts "Please enter the defence of your character (0-75)"
-        puts "--------------------------------------------------"
+            puts "--------------------------------------------------"
+            puts "Please enter the defence of your character (0-75)"
+            puts "--------------------------------------------------"
         ans_defence = gets.chomp
 
         @new_character = Character.find_or_create_by(name: ans_name, species: ans_species, health: ans_health, strength: ans_strength, defence: ans_defence, user_id: @new_user.id)
@@ -86,27 +86,58 @@ class CommandLineInterface
     def view_warriors
         ux = User.find(@new_user.id)
         uw = ux.characters
-        warriors = uw.map{|character| character.name }
-        puts warriors
+        puts "--------------------------"
+        puts "These are your characters:"
+        puts "--------------------------"
+        i = 0 
+        c = uw.map{|character| character} 
+        while c.count > i 
+            puts "#{c[i][:name]} - Species: #{c[i][:species]} - Health: #{c[i][:health]} - Strength #{c[i][:strength]} - Defence #{c[i][:defence]}"
+            i += 1
+        end 
+        puts "-------------------------------------------------------------"
+        
+
+            
     end
 
-    # def gear_up
-    #     self.all.map{|weapon| weapon.name }
-    #     puts "Which weapon would you like to equip?"
-    #     ans = gets.chomp 
-    #     ans_weapon = Weapon.find_by(name: ans)
-    #     new_cw = CharacterWeapon.find_or_create_by(character_id: character.id, weapon_id: ans_weapon.id)
-    # end
+    def gear_up
+        view_warriors
+            puts "-------------------------------------------"
+            puts "Choose your character you'd like to upgrade"
+            puts "-------------------------------------------"
+        choice = gets.chomp
+        char = Character.find_by(name: choice)
+
+            i = 0
+            w = Weapon.all.map{|weapon| weapon}
+        while w.count > i
+            puts "----------------------------------------------------------------------------"
+            puts "Weapon: #{w[i][:name]} - #{w[i][:category]} - Health: #{w[i][:health]} - Strength: #{w[i][:strength]} - Defence: #{w[i][:defence]}"
+            puts "----------------------------------------------------------------------------"
+            i += 1
+        end 
+        
+            puts "--------------------------------------------"
+            puts "Which weapon would you like to equip? (name)"
+            puts "--------------------------------------------"
+        ans = gets.chomp 
+        ans_weapon = Weapon.find_by(name: ans)
+        new_health = char.health + ans_weapon.health
+        new_strength = char.strength + ans_weapon.strength 
+        new_defence = char.defence + ans_weapon.defence 
+
+            puts "----------------------------------------------------"
+            puts "#{char.name} now has #{new_health} health, #{new_strength} strength and #{new_defence} defence!"
+            puts "----------------------------------------------------"
+
+    end
 
     def kys
-        # ux = User.find(@new_user.id)
-        # uw = ux.characters
-        # warriors = uw.map{|character| character.name }
-        # puts warriors 
-        puts view_warriors
-        puts "--------------------------------------------------"
-        puts "Please choose the character you would like to kill"
-        puts "--------------------------------------------------"
+            puts view_warriors
+            puts "--------------------------------------------------"
+            puts "Please choose the character you would like to kill"
+            puts "--------------------------------------------------"
         ans_kill = gets.chomp  
         ans_kys = Character.find_by(name: ans_kill)
         if ans_kys.nil? 
